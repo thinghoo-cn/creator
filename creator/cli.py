1bb1118868c239e352b28cc74599392a3024741c
svtter <svtter@qq.com>
2020-06-25 11:40:01 +0800

    format code with pyfmt

--- creator/cli.py
+++ creator/cli.py
@@ -1,5 +1,5 @@
-
 import click
+
 from . import libs
 
 
@@ -9,25 +9,24 @@ def cli():
 
 
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
+@click.option("--count", default=1, help="Number of greetings.")
+@click.option("--name", prompt="Your name", help="The person to greet.")
 def hello(count, name):
     """Simple program that greets NAME for a total of COUNT times."""
     for x in range(count):
-        click.echo('Hello %s!' % name)
+        click.echo("Hello %s!" % name)
 
 
 @click.command()
-@click.option('--package_name', default='', help='package name')
+@click.option("--package_name", default="", help="package name")
 def create_package(package_name):
     """create python package command
 
     Args:
         package_name (str): name of package
     """
-    if package_name == '':
-        raise Exception('empty package name.')
+    if package_name == "":
+        raise Exception("empty package name.")
     libs.create_package(package_name=package_name)
 
 
--- creator/libs.py
+++ creator/libs.py
@@ -5,12 +5,11 @@ import pathlib
 def create_package(package_name: str) -> pathlib.Path:
     path = pathlib.Path(package_name)
     if path.exists():
-        raise Exception('not work')
+        raise Exception("not work")
 
     path = path.absolute()
     os.mkdir(path)
-    initpy = path / '__init__.py'
+    initpy = path / "__init__.py"
     initpy.touch()
 
     return path
-    
